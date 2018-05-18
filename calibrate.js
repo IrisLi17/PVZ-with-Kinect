d4add291efc62b82c28959d14f7f7b0c04f7aab1
Iris Li <461729541@qq.com>
2018-05-19 00:26:04 +0800

    ugly calibrate page

---
+++ calibrate.js
@@ -0,0 +1,16 @@
+document.getElementById("point1").addEventListener("click",()=>{
+    document.getElementById("point1").style.backgroundColor = "#000000";
+    //alert("click1");
+})
+document.getElementById("point2").addEventListener("click",()=>{
+    document.getElementById("point2").style.backgroundColor = "#000000";
+    //alert("click1");
+})
+document.getElementById("point3").addEventListener("click",()=>{
+    document.getElementById("point3").style.backgroundColor = "#000000";
+    //alert("click1");
+})
+document.getElementById("point4").addEventListener("click",()=>{
+    document.getElementById("point4").style.backgroundColor = "#000000";
+    //alert("click1");
+})
